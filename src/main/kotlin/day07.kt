ecde492c63f6b965fb0fa28c7adba2b3edd5f623
Eliott RGX <eliott.rogeaux@gmail.com>
2024-12-08 19:50:30 +0100

    Day 07

--- src/main/kotlin/day07.kt
+++ src/main/kotlin/day07.kt
@@ -1,15 +1,12 @@
-import java.math.BigInteger
-
 fun main() = aoc(7) { input ->
-    val pairs = input.lines().map { it.split(": ") }.map { it[0].toBigInteger() to it[1].split(" ").map(String::toBigInteger) }
+    val pairs = input.lines().map { it.split(": ") }.map { it[0].toLong() to it[1].split(" ").map(String::toLong) }
 
     pairs.map { (total, numbers) ->
-        numbers.foldIndexed(listOf<BigInteger>() to listOf<BigInteger>()) { i, (last, last2), n ->
-            if (i == 0) listOf(n) to listOf(n) else {
-                (last.map { it + n } + last.map { it * n } to
-                        last2.map { it + n } + last2.map { it * n } + last2.map { BigInteger(it.toString() + n.toString()) })
-            }
+        numbers.foldIndexed(listOf<Long>() to listOf<Long>()) { i, (last, last2), n ->
+            if (i == 0) listOf(n) to listOf(n)
+            else (last.map { it + n } + last.map { it * n } to last2.map { it + n } + last2.map { it * n } + last2.map { (it.toString() + n.toString()).toLong() })
+        }.let { (totals, totals2) ->
+            (if (totals.find { it == total } != null) total else 0L) to (if (totals2.find { it == total } != null) total else 0L)
         }
-            .let { (totals, totals2) -> (if (totals.find { it == total } != null) total else BigInteger.ZERO) to (if (totals2.find { it == total } != null) total else BigInteger.ZERO) }
-    }.unzip().let { (part1, part2) -> part1.fold(BigInteger.ZERO) { acc, i -> acc + i } to part2.fold(BigInteger.ZERO) { acc, i -> acc + i } }
+    }.unzip().let { (part1, part2) -> part1.sum() to part2.sum() }
 }
